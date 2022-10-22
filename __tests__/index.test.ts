ce457d4ef3d72cda00a2aa201ffd3108ad963cc4
JeyDev310 <mtroz310@gmail.com>
2022-10-23 03:26:31 +0800

    feat: updated tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,36 +1,26 @@
 // import request from 'supertest';
 import request from 'superagent';
 
-import app from '../src/app';
-import sequelize from '../src//constants/db.init';
-
-// let connection, server;
-const host = 'localhost:8000'
-
-beforeEach(async () => {
-  //db connection
-  // const connection = await sequelize.sync();
-  // connection
-
-  // server = app.listen(8000);
-});
-
-afterEach(() => {
-  // server.close();
-});
+const host = 'localhost:8000';
 
 afterAll((done) => {
   done();
-})
+});
 
 describe('Endpoints tests', () => {
   it('Get top 20 articles: [/articles]', async() => {
-    // const response = await request(app).get('/');
-    // expect(response.statusCode).toBe(200);
-
     const response = await request.get(`${host}/articles`);
     expect(response.statusCode).toBe(200);
-    expect(response.body.list).toBe(20);
+    expect(response.body.list.length).toBe(20);
+  });
+
+  it('Get article by id: [/articles/:id]', async() => {
+    const listRes = await request.get(`${host}/articles`);
+    expect(listRes.statusCode).toBe(200);
+    expect(listRes.body.list.length).toBe(20);
+
+    const id = listRes.body.list[0].id;
+    const response = await request.get(`${host}/articles/${id}`);
+    expect(response.statusCode).toBe(200);
   });
 });
-
